e0b32523ddaa195534ffaefc8e07201481270d98
pcolor999 <109137873+pcolor999@users.noreply.github.com>
2026-03-30 10:49:05 +0000

    Add print text

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("Hello, world!");
-}
+
+    use std::{thread, time};
+    let duration = time::Duration::from_millis(100);
+    for i in 0..10 {
+        println!("{}", i);
+        thread::sleep(duration);
+    }
+}   
